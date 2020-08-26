2e7850840f38ea277037d77c22594604784fa9e7
Aaron Madlon-Kay <aaron@madlon-kay.com>
2020-08-26 21:30:23 +0900

    Revert "Fix unwanted scrolling caused by SelectableText"
    This reverts commit 1d8525e9f363c38c32b97f0af45b656de0228738.

--- lib/src/util/widget.dart
+++ lib/src/util/widget.dart
@@ -53,7 +53,6 @@ Widget maybeSelectableText(
     return SelectableText.rich(
       span as TextSpan,
       textAlign: textAlign,
-      scrollPhysics: const NeverScrollableScrollPhysics(),
     );
   } else {
     return Text.rich(
