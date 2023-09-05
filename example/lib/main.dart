06a18cf5957da2ce08e561cb92b71dc2574ae786
Aaron Madlon-Kay <aaron@madlon-kay.com>
2023-09-05 23:20:41 +0900

    Add editing example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -27,15 +27,86 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('org_flutter'),
-      ),
-      body: const Org(
-        '''* TODO [#A] foo bar
-baz buzz''',
-        restorationId: 'my_org_widget',
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('org_flutter'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Simple'),
+              Tab(text: 'Complex'),
+            ],
+          ),
+        ),
+        body: const TabBarView(children: [
+          SimpleTab(),
+          ComplexTab(),
+        ]),
       ),
     );
+  }
+}
+
+class SimpleTab extends StatelessWidget {
+  const SimpleTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Org(
+      '''* TODO [#A] foo bar
+baz buzz''',
+      restorationId: 'my_org_widget',
+    );
+  }
+}
+
+class ComplexTab extends StatefulWidget {
+  const ComplexTab({Key? key}) : super(key: key);
+
+  @override
+  State<ComplexTab> createState() => _ComplexTabState();
+}
+
+class _ComplexTabState extends State<ComplexTab> {
+  late OrgDocument root;
+
+  @override
+  void initState() {
+    root = OrgDocument.parse('''* TODO [#A] foo bar
+~1~''');
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return OrgController(
+      root: root,
+      child: ListView(
+        children: [
+          OrgRootWidget(child: OrgDocumentWidget(root, shrinkWrap: true)),
+          ElevatedButton(
+            onPressed: _incrementCounter,
+            child: const Text('Increment'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _incrementCounter() {
+    late OrgMarkup markupNode;
+    root.visit<OrgMarkup>((node) {
+      markupNode = node;
+      return false; // stop visiting
+    });
+    final value = int.parse(markupNode.content);
+    setState(() {
+      root = root
+          .editNode(markupNode)!
+          .replace(OrgMarkup('~', '${value + 1}', '~', OrgStyle.code))
+          .commit() as OrgDocument;
+    });
   }
 }
