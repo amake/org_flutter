def724ebc1503c8d107f8d3c4ca75cd463194a05
Aaron Madlon-Kay <aaron@madlon-kay.com>
2023-12-04 23:16:18 +0900

    Regroup unit tests

--- test/org_flutter_test.dart
+++ test/org_flutter_test.dart
@@ -119,161 +119,162 @@ bar
         expect(invoked, isTrue);
       });
     });
-    group('Local variables', () {
-      testWidgets('Custom entities', (tester) async {
-        final doc = OrgDocument.parse(r'''
+  });
+  group('Local variables', () {
+    testWidgets('Custom entities', (tester) async {
+      final doc = OrgDocument.parse(r'''
 foo \pineapple bar
 
 # Local Variables:
 # org-entities-user: (("pineapple" "[p]" nil "&#127821;" "[p]" "[p]" "🍍"))
 # End:
 ''');
-        final widget = OrgController(
-          root: doc,
-          interpretEmbeddedSettings: true,
-          errorHandler: (e) {
-            fail(e.toString());
-          },
-          child: OrgRootWidget(
-            child: OrgDocumentWidget(doc),
-          ),
-        );
-        await tester.pumpWidget(_wrap(widget));
-        expect(find.textContaining('foo \u{1f34d} bar'), findsOneWidget);
-      });
-      testWidgets('Disable entities', (tester) async {
-        final doc = OrgDocument.parse(r'''
+      final widget = OrgController(
+        root: doc,
+        interpretEmbeddedSettings: true,
+        errorHandler: (e) {
+          fail(e.toString());
+        },
+        child: OrgRootWidget(
+          child: OrgDocumentWidget(doc),
+        ),
+      );
+      await tester.pumpWidget(_wrap(widget));
+      expect(find.textContaining('foo \u{1f34d} bar'), findsOneWidget);
+    });
+    testWidgets('Disable entities', (tester) async {
+      final doc = OrgDocument.parse(r'''
 foo \smiley bar
 
 # Local Variables:
 # org-pretty-entities: nil
 # End:
 ''');
-        final widget = OrgController(
-          root: doc,
-          interpretEmbeddedSettings: true,
-          errorHandler: (e) {
-            fail(e.toString());
-          },
-          child: OrgRootWidget(
-            child: OrgDocumentWidget(doc),
-          ),
-        );
-        await tester.pumpWidget(_wrap(widget));
-        expect(find.textContaining('foo'), findsOneWidget);
-        expect(find.textContaining('☺'), findsNothing);
-      });
-      testWidgets('Hide markup', (tester) async {
-        final doc = OrgDocument.parse(r'''
+      final widget = OrgController(
+        root: doc,
+        interpretEmbeddedSettings: true,
+        errorHandler: (e) {
+          fail(e.toString());
+        },
+        child: OrgRootWidget(
+          child: OrgDocumentWidget(doc),
+        ),
+      );
+      await tester.pumpWidget(_wrap(widget));
+      expect(find.textContaining('foo'), findsOneWidget);
+      expect(find.textContaining('☺'), findsNothing);
+    });
+    testWidgets('Hide markup', (tester) async {
+      final doc = OrgDocument.parse(r'''
 foo *bar* baz
 
 # Local Variables:
 # org-hide-emphasis-markers: t
 # End:
 ''');
-        final widget = OrgController(
-          root: doc,
-          interpretEmbeddedSettings: true,
-          errorHandler: (e) {
-            fail(e.toString());
-          },
-          child: OrgRootWidget(
-            child: OrgDocumentWidget(doc),
-          ),
-        );
-        await tester.pumpWidget(_wrap(widget));
-        expect(find.textContaining('foo bar baz'), findsOneWidget);
-        expect(find.textContaining('foo *bar* baz'), findsNothing);
-      });
+      final widget = OrgController(
+        root: doc,
+        interpretEmbeddedSettings: true,
+        errorHandler: (e) {
+          fail(e.toString());
+        },
+        child: OrgRootWidget(
+          child: OrgDocumentWidget(doc),
+        ),
+      );
+      await tester.pumpWidget(_wrap(widget));
+      expect(find.textContaining('foo bar baz'), findsOneWidget);
+      expect(find.textContaining('foo *bar* baz'), findsNothing);
     });
-    group('Keyword settings', () {
-      testWidgets('Blocks start closed', (tester) async {
-        final doc = OrgDocument.parse(r'''
+  });
+  group('Keyword settings', () {
+    testWidgets('Blocks start closed', (tester) async {
+      final doc = OrgDocument.parse(r'''
 #+begin_example
 foo bar
 #+end_example
 
 #+STARTUP: hideblocks
 ''');
-        final widget = OrgController(
-          root: doc,
-          interpretEmbeddedSettings: true,
-          errorHandler: (e) {
-            fail(e.toString());
-          },
-          child: OrgRootWidget(
-            child: OrgDocumentWidget(doc),
-          ),
-        );
-        await tester.pumpWidget(_wrap(widget));
-        expect(find.textContaining('foo bar'), findsNothing);
-      });
-      testWidgets('Drawers start open', (tester) async {
-        final doc = OrgDocument.parse(r'''
+      final widget = OrgController(
+        root: doc,
+        interpretEmbeddedSettings: true,
+        errorHandler: (e) {
+          fail(e.toString());
+        },
+        child: OrgRootWidget(
+          child: OrgDocumentWidget(doc),
+        ),
+      );
+      await tester.pumpWidget(_wrap(widget));
+      expect(find.textContaining('foo bar'), findsNothing);
+    });
+    testWidgets('Drawers start open', (tester) async {
+      final doc = OrgDocument.parse(r'''
 :PROPERTIES:
 :foo: bar
 :END:
 
 #+STARTUP: nohidedrawers
 ''');
-        final widget = OrgController(
-          root: doc,
-          interpretEmbeddedSettings: true,
-          errorHandler: (e) {
-            fail(e.toString());
-          },
-          child: OrgRootWidget(
-            child: OrgDocumentWidget(doc),
-          ),
-        );
-        await tester.pumpWidget(_wrap(widget));
-        expect(find.textContaining(':foo: bar'), findsOneWidget);
-      });
-      testWidgets('Sections start open', (tester) async {
-        final doc = OrgDocument.parse(r'''
+      final widget = OrgController(
+        root: doc,
+        interpretEmbeddedSettings: true,
+        errorHandler: (e) {
+          fail(e.toString());
+        },
+        child: OrgRootWidget(
+          child: OrgDocumentWidget(doc),
+        ),
+      );
+      await tester.pumpWidget(_wrap(widget));
+      expect(find.textContaining(':foo: bar'), findsOneWidget);
+    });
+    testWidgets('Sections start open', (tester) async {
+      final doc = OrgDocument.parse(r'''
 foo
 
 * bar
   baz
 #+STARTUP: showeverything
 ''');
-        final widget = OrgController(
-          root: doc,
-          interpretEmbeddedSettings: true,
-          errorHandler: (e) {
-            fail(e.toString());
-          },
-          child: OrgRootWidget(
-            child: OrgDocumentWidget(doc),
-          ),
-        );
-        await tester.pumpWidget(_wrap(widget));
-        expect(find.textContaining('baz'), findsOneWidget);
-      });
-      testWidgets('Sections start closed', (tester) async {
-        final doc = OrgDocument.parse(r'''
+      final widget = OrgController(
+        root: doc,
+        interpretEmbeddedSettings: true,
+        errorHandler: (e) {
+          fail(e.toString());
+        },
+        child: OrgRootWidget(
+          child: OrgDocumentWidget(doc),
+        ),
+      );
+      await tester.pumpWidget(_wrap(widget));
+      expect(find.textContaining('baz'), findsOneWidget);
+    });
+    testWidgets('Sections start closed', (tester) async {
+      final doc = OrgDocument.parse(r'''
 foo
 
 * bar
   baz
 #+STARTUP: overview
 ''');
-        final widget = OrgController(
-          root: doc,
-          interpretEmbeddedSettings: true,
-          errorHandler: (e) {
-            fail(e.toString());
-          },
-          child: OrgRootWidget(
-            child: OrgDocumentWidget(doc),
-          ),
-        );
-        await tester.pumpWidget(_wrap(widget));
-        expect(find.textContaining('baz'), findsNothing);
-      });
-      testWidgets('Showeverything overrides hideblocks, hidedrawers',
-          (tester) async {
-        final doc = OrgDocument.parse(r'''
+      final widget = OrgController(
+        root: doc,
+        interpretEmbeddedSettings: true,
+        errorHandler: (e) {
+          fail(e.toString());
+        },
+        child: OrgRootWidget(
+          child: OrgDocumentWidget(doc),
+        ),
+      );
+      await tester.pumpWidget(_wrap(widget));
+      expect(find.textContaining('baz'), findsNothing);
+    });
+    testWidgets('Showeverything overrides hideblocks, hidedrawers',
+        (tester) async {
+      final doc = OrgDocument.parse(r'''
 :PROPERTIES:
 :foo: bar
 :END:
@@ -284,84 +285,83 @@ biz baz
 
 #+STARTUP: showeverything hideblocks hidedrawers
 ''');
-        final widget = OrgController(
-          root: doc,
-          interpretEmbeddedSettings: true,
-          errorHandler: (e) {
-            fail(e.toString());
-          },
-          child: OrgRootWidget(
-            child: OrgDocumentWidget(doc),
-          ),
-        );
-        await tester.pumpWidget(_wrap(widget));
-        expect(find.textContaining(':foo: bar'), findsOneWidget);
-        expect(find.textContaining('biz baz'), findsOneWidget);
-      });
-      testWidgets('Hide stars', (tester) async {
-        final doc = OrgDocument.parse(r'''
+      final widget = OrgController(
+        root: doc,
+        interpretEmbeddedSettings: true,
+        errorHandler: (e) {
+          fail(e.toString());
+        },
+        child: OrgRootWidget(
+          child: OrgDocumentWidget(doc),
+        ),
+      );
+      await tester.pumpWidget(_wrap(widget));
+      expect(find.textContaining(':foo: bar'), findsOneWidget);
+      expect(find.textContaining('biz baz'), findsOneWidget);
+    });
+    testWidgets('Hide stars', (tester) async {
+      final doc = OrgDocument.parse(r'''
 * foo
 ** bar
 *** baz
 
 #+STARTUP: showeverything hidestars
 ''');
-        final widget = OrgController(
-          root: doc,
-          interpretEmbeddedSettings: true,
-          errorHandler: (e) {
-            fail(e.toString());
-          },
-          child: OrgRootWidget(
-            child: OrgDocumentWidget(doc),
-          ),
-        );
-        await tester.pumpWidget(_wrap(widget));
-        expect(find.textContaining('* foo'), findsOneWidget);
-        expect(find.textContaining('** bar'), findsNothing);
-        expect(find.textContaining(' * bar'), findsOneWidget);
-        expect(find.textContaining('*** baz'), findsNothing);
-        expect(find.textContaining('  * baz'), findsOneWidget);
-      });
-      testWidgets('Disable entities', (tester) async {
-        final doc = OrgDocument.parse(r'''
+      final widget = OrgController(
+        root: doc,
+        interpretEmbeddedSettings: true,
+        errorHandler: (e) {
+          fail(e.toString());
+        },
+        child: OrgRootWidget(
+          child: OrgDocumentWidget(doc),
+        ),
+      );
+      await tester.pumpWidget(_wrap(widget));
+      expect(find.textContaining('* foo'), findsOneWidget);
+      expect(find.textContaining('** bar'), findsNothing);
+      expect(find.textContaining(' * bar'), findsOneWidget);
+      expect(find.textContaining('*** baz'), findsNothing);
+      expect(find.textContaining('  * baz'), findsOneWidget);
+    });
+    testWidgets('Disable entities', (tester) async {
+      final doc = OrgDocument.parse(r'''
 foo \smiley bar
 
 #+STARTUP: entitiesplain
 ''');
-        final widget = OrgController(
-          root: doc,
-          interpretEmbeddedSettings: true,
-          errorHandler: (e) {
-            fail(e.toString());
-          },
-          child: OrgRootWidget(
-            child: OrgDocumentWidget(doc),
-          ),
-        );
-        await tester.pumpWidget(_wrap(widget));
-        expect(find.textContaining('foo'), findsOneWidget);
-        expect(find.textContaining('☺'), findsNothing);
-      });
-      testWidgets('Disable inline images', (tester) async {
-        final doc = OrgDocument.parse(r'''
+      final widget = OrgController(
+        root: doc,
+        interpretEmbeddedSettings: true,
+        errorHandler: (e) {
+          fail(e.toString());
+        },
+        child: OrgRootWidget(
+          child: OrgDocumentWidget(doc),
+        ),
+      );
+      await tester.pumpWidget(_wrap(widget));
+      expect(find.textContaining('foo'), findsOneWidget);
+      expect(find.textContaining('☺'), findsNothing);
+    });
+    testWidgets('Disable inline images', (tester) async {
+      final doc = OrgDocument.parse(r'''
 file:./foo.png
 
 #+STARTUP: noinlineimages
 ''');
-        final widget = OrgController(
-          root: doc,
-          interpretEmbeddedSettings: true,
-          errorHandler: (e) {
-            fail(e.toString());
-          },
-          child: OrgRootWidget(
-            loadImage: (_) => fail('Should not be invoked'),
-            child: OrgDocumentWidget(doc),
-          ),
-        );
-        await tester.pumpWidget(_wrap(widget));
-      });
+      final widget = OrgController(
+        root: doc,
+        interpretEmbeddedSettings: true,
+        errorHandler: (e) {
+          fail(e.toString());
+        },
+        child: OrgRootWidget(
+          loadImage: (_) => fail('Should not be invoked'),
+          child: OrgDocumentWidget(doc),
+        ),
+      );
+      await tester.pumpWidget(_wrap(widget));
     });
   });
 }
