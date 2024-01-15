3ee2996d8c4162e1e0e54919825ade588d000ecd
Aaron Madlon-Kay <aaron@madlon-kay.com>
2024-01-16 08:42:22 +0900

    Add unit tests for state restoration

--- test/org_flutter_test.dart
+++ test/org_flutter_test.dart
@@ -99,6 +99,91 @@ bazinga''')));
         expect(find.textContaining('#+end_example'), findsNothing);
       });
     });
+    group('State restoration', () {
+      testWidgets('No restoration ID', (tester) async {
+        await tester.pumpWidget(RootRestorationScope(
+          restorationId: 'root',
+          child: _wrap(const Org('''
+foo bar
+* headline 1
+baz buzz
+** headline 2
+bazinga''')),
+        ));
+        expect(find.text('foo bar'), findsOneWidget);
+        await tester.tap(find.byType(OrgHeadlineWidget).first);
+        await tester.pump();
+        expect(find.text('foo bar'), findsOneWidget);
+        expect(find.text('baz buzz'), findsOneWidget);
+        expect(find.textContaining('headline 2'), findsOneWidget);
+        expect(find.text('bazinga'), findsNothing);
+        await tester.restartAndRestore();
+        expect(find.text('foo bar'), findsOneWidget);
+        expect(find.text('baz buzz'), findsNothing);
+        expect(find.textContaining('headline 2'), findsNothing);
+        expect(find.text('bazinga'), findsNothing);
+      });
+      testWidgets('Restores section visibility', (tester) async {
+        await tester.pumpWidget(RootRestorationScope(
+          restorationId: 'root',
+          child: _wrap(const Org(
+            '''
+foo bar
+* headline 1
+baz buzz
+** headline 2
+bazinga''',
+            restorationId: 'doc',
+          )),
+        ));
+        expect(find.text('foo bar'), findsOneWidget);
+        await tester.tap(find.byType(OrgHeadlineWidget).first);
+        await tester.pump();
+        expect(find.text('foo bar'), findsOneWidget);
+        expect(find.text('baz buzz'), findsOneWidget);
+        expect(find.textContaining('headline 2'), findsOneWidget);
+        expect(find.text('bazinga'), findsNothing);
+        await tester.restartAndRestore();
+        expect(find.text('foo bar'), findsOneWidget);
+        expect(find.text('baz buzz'), findsOneWidget);
+        expect(find.textContaining('headline 2'), findsOneWidget);
+        expect(find.text('bazinga'), findsNothing);
+      });
+      testWidgets('Ignores search on restore', (tester) async {
+        final doc = OrgDocument.parse('''foo bar
+* headline 1
+baz buzz
+** headline 2
+bazinga''');
+        final widget = OrgController(
+          root: doc,
+          searchQuery: RegExp('baz'),
+          restorationId: 'doc',
+          child: OrgRootWidget(
+            child: OrgDocumentWidget(doc),
+          ),
+        );
+        await tester.pumpWidget(RootRestorationScope(
+          restorationId: 'root',
+          child: _wrap(widget),
+        ));
+        expect(find.text('foo bar'), findsOneWidget);
+        expect(find.textContaining('buzz'), findsOneWidget);
+        expect(find.textContaining('headline 2'), findsOneWidget);
+        expect(find.textContaining('inga'), findsOneWidget);
+        await tester.tap(find.byType(OrgHeadlineWidget).last);
+        await tester.pumpAndSettle();
+        expect(find.text('foo bar'), findsOneWidget);
+        expect(find.textContaining('buzz'), findsOneWidget);
+        expect(find.textContaining('headline 2'), findsOneWidget);
+        expect(find.textContaining('inga'), findsNothing);
+        await tester.restartAndRestore();
+        expect(find.text('foo bar'), findsOneWidget);
+        expect(find.textContaining('buzz'), findsOneWidget);
+        expect(find.textContaining('headline 2'), findsOneWidget);
+        expect(find.textContaining('inga'), findsNothing);
+      });
+    });
     group('Events', () {
       testWidgets('Load images', (tester) async {
         var invoked = false;
