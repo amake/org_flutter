815cfc2e70f2d883cd4eaf44d878ddf2861ed821
Aaron Madlon-Kay <aaron@madlon-kay.com>
2024-10-09 08:10:03 +0900

    Fix extra space after OrgDrawerWidget
    See https://github.com/amake/orgro/issues/107

--- lib/src/widgets.dart
+++ lib/src/widgets.dart
@@ -1065,39 +1065,36 @@ class _OrgDrawerWidgetState extends State<OrgDrawerWidget>
 
   @override
   Widget build(BuildContext context) {
-    final defaultStyle = DefaultTextStyle.of(context).style;
-    final drawerStyle =
-        defaultStyle.copyWith(color: OrgTheme.dataOf(context).drawerColor);
     final body = IndentBuilder(
       widget.drawer.indent,
       builder: (context, totalIndentSize) {
+        final defaultStyle = DefaultTextStyle.of(context).style;
+        final drawerStyle =
+            defaultStyle.copyWith(color: OrgTheme.dataOf(context).drawerColor);
         return ValueListenableBuilder<bool>(
           valueListenable: openListenable,
-          builder: (context, open, child) => Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              InkWell(
-                onTap: () => openListenable.value = !openListenable.value,
-                child: Text(
-                  widget.drawer.header.trimRight() + (open ? '' : '...'),
-                  style: drawerStyle,
+          builder: (context, open, child) {
+            final trailingWidget = _trailing();
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                InkWell(
+                  onTap: () => openListenable.value = !openListenable.value,
+                  child: Text(
+                    widget.drawer.header.trimRight() + (open ? '' : '...'),
+                    style: drawerStyle,
+                  ),
                 ),
-              ),
-              AnimatedSwitcher(
-                duration: const Duration(milliseconds: 100),
-                transitionBuilder: (child, animation) =>
-                    SizeTransition(sizeFactor: animation, child: child),
-                child: open ? child : const SizedBox.shrink(),
-              ),
-              // Remove two linebreaks because we introduce two by splitting the
-              // text into two widgets in this Column
-              Text(
-                removeTrailingLineBreak(
-                  removeTrailingLineBreak(widget.drawer.trailing),
+                AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 100),
+                  transitionBuilder: (child, animation) =>
+                      SizeTransition(sizeFactor: animation, child: child),
+                  child: open ? child : const SizedBox.shrink(),
                 ),
-              )
-            ],
-          ),
+                if (trailingWidget != null) trailingWidget,
+              ],
+            );
+          },
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
@@ -1131,6 +1128,17 @@ class _OrgDrawerWidgetState extends State<OrgDrawerWidget>
             physics: const AlwaysScrollableScrollPhysics(),
             child: body,
           );
+  }
+
+  Widget? _trailing() {
+    var trailing = removeTrailingLineBreak(widget.drawer.trailing);
+    // If trailing is empty here then there is something immediately following
+    // the drawer. Because we render the drawer with full width, any trailing
+    // Text widget will result in an unwanted empty line. Thus we return null.
+    if (trailing.isEmpty) return null;
+    // We have to remove another linebreak because there will be an implicit
+    // linebreak when this widget ends.
+    return Text(removeTrailingLineBreak(trailing));
   }
 }
 
