eb2eef488b32a613bcb2bd6f0ca08eb247491180
Aaron Madlon-Kay <aaron@madlon-kay.com>
2024-10-20 23:34:36 +0900

    Handle separated trailing space in OrgParagraph

--- lib/src/widget/org_paragraph.dart
+++ lib/src/widget/org_paragraph.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:org_flutter/src/controller.dart';
 import 'package:org_flutter/src/indent.dart';
 import 'package:org_flutter/src/settings.dart';
+import 'package:org_flutter/src/span.dart';
 import 'package:org_flutter/src/util/util.dart';
-import 'package:org_flutter/src/widget/org_content.dart';
 import 'package:org_parser/org_parser.dart';
 
 /// An Org Mode paragraph
@@ -17,34 +17,43 @@ class OrgParagraphWidget extends StatelessWidget {
     return IndentBuilder(
       paragraph.indent,
       builder: (context, totalIndentSize) {
-        return OrgContentWidget(
-          paragraph.body,
-          transformer: (elem, content) {
-            final isLast = identical(elem, paragraph.body.children.last);
-            var formattedContent = deindent(content, totalIndentSize);
-            if (reflow) {
-              formattedContent = reflowText(formattedContent, end: isLast);
-            }
-            if (isLast) {
-              final last = removeTrailingLineBreak(formattedContent);
-              // A trailing linebreak results in a line with the same height as
-              // the previous line. This is bad when the previous line is
-              // artificially tall due to a WidgetSpan (especially an image). To
-              // avoid this we add a zero-width space to the end if the text has
-              // a single, trailing linebreak.
-              //
-              // See: https://github.com/flutter/flutter/issues/156268
-              //
-              // TODO(aaron): Limit to when the previous element is a link?
-              return last.indexOf('\n') == last.length - 1
-                  ? '$last\u200b'
-                  : last;
-            } else {
-              return formattedContent;
-            }
-          },
+        return FancySpanBuilder(
+          builder: (context, spanBuilder) => Text.rich(TextSpan(children: [
+            spanBuilder.build(
+              paragraph.body,
+              transformer: (elem, content) {
+                final isLast = identical(elem, paragraph.body.children.last);
+                var formattedContent = deindent(content, totalIndentSize);
+                if (reflow) {
+                  formattedContent = reflowText(formattedContent, end: isLast);
+                }
+                if (isLast && paragraph.trailing.isEmpty) {
+                  formattedContent = removeTrailingLineBreak(formattedContent);
+                }
+                return formattedContent;
+              },
+            ),
+            if (paragraph.trailing.isNotEmpty) _trailingSpan(),
+          ])),
         );
       },
     );
+  }
+
+  TextSpan _trailingSpan() {
+    var trailing = removeTrailingLineBreak(paragraph.trailing);
+    // A trailing linebreak results in a line with the same height as
+    // the previous line. This is bad when the previous line is
+    // artificially tall due to a WidgetSpan (especially an image). To
+    // avoid this we add a zero-width space to the end if the text has
+    // a single, trailing linebreak.
+    //
+    // See: https://github.com/flutter/flutter/issues/156268
+    //
+    // TODO(aaron): Limit to when the previous element is a link?
+    if (trailing.indexOf('\n') == trailing.length - 1) {
+      trailing = '$trailing\u200b';
+    }
+    return TextSpan(text: trailing);
   }
 }
