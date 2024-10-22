225b1b2657becc5ee9ece7061b824c57bf5b71d0
Aaron Madlon-Kay <aaron@madlon-kay.com>
2024-10-23 07:29:43 +0900

    Add bidi tests

---
+++ test/bidi_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:org_flutter/src/util/bidi.dart';
+import 'package:org_parser/org_parser.dart';
+
+void main() {
+  test('detect ltr', () {
+    final node = OrgPlainText('foo');
+    final textDirection = node.detectTextDirection();
+    expect(textDirection, TextDirection.ltr);
+  });
+  test('detect rtl', () {
+    final node = OrgPlainText('אבج');
+    final textDirection = node.detectTextDirection();
+    expect(textDirection, TextDirection.rtl);
+  });
+  test('detect unknown', () {
+    final node = OrgPlainText('123');
+    final textDirection = node.detectTextDirection();
+    expect(textDirection, isNull);
+  });
+}
