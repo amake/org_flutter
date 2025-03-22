e51d7249f601dc6c735c21c295d2c506d9adad4d
Aaron Madlon-Kay <aaron@madlon-kay.com>
2025-03-22 22:30:10 +0900

    Use relative paths for export

--- lib/src/util/util.dart
+++ lib/src/util/util.dart
@@ -1,8 +1,8 @@
-export 'package:org_flutter/src/util/bidi.dart';
-export 'package:org_flutter/src/util/collection.dart';
-export 'package:org_flutter/src/util/keywords.dart';
-export 'package:org_flutter/src/util/local_variables.dart';
-export 'package:org_flutter/src/util/locale.dart';
-export 'package:org_flutter/src/util/text.dart';
-export 'package:org_flutter/src/util/value_notifier.dart';
-export 'package:org_flutter/src/util/widget.dart';
+export './bidi.dart';
+export './collection.dart';
+export './keywords.dart';
+export './local_variables.dart';
+export './locale.dart';
+export './text.dart';
+export './value_notifier.dart';
+export './widget.dart';
