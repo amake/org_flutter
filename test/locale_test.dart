6d21f35902773de4a34be4e2f676147ce887f42a
Aaron Madlon-Kay <aaron@madlon-kay.com>
2025-03-22 22:34:22 +0900

    Expand locale tests

--- test/locale_test.dart
+++ test/locale_test.dart
@@ -1,64 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:org_flutter/src/util/util.dart';
+import 'package:org_parser/org_parser.dart';
 
 void main() {
-  test('language', () {
-    expect(
-      tryParseLocale('en'),
-      Locale('en'),
-    );
-  });
-  test('language and region', () {
-    expect(
-      tryParseLocale('en_US'),
-      Locale('en', 'US'),
-    );
-  });
-  test('language and script', () {
-    expect(
-      tryParseLocale('zh_Hans'),
-      Locale.fromSubtags(languageCode: 'zh', scriptCode: 'Hans'),
-    );
-  });
-  test('language, script, and region', () {
-    expect(
-      tryParseLocale('zh_Hans_CN'),
-      Locale.fromSubtags(
-        languageCode: 'zh',
-        scriptCode: 'Hans',
-        countryCode: 'CN',
-      ),
-    );
-  });
-  test('hyphens', () {
-    expect(
-      tryParseLocale('zh-Hans-CN'),
-      Locale.fromSubtags(
-        languageCode: 'zh',
-        scriptCode: 'Hans',
-        countryCode: 'CN',
-      ),
-    );
-  });
-  group('invalid', () {
+  group('extract', () {
+    test('simple', () {
+      final doc = OrgDocument.parse('''
+#+LANGUAGE: en
+foo
+''');
+      expect(extractLocale(doc), Locale('en'));
+    });
+    test('case-insensitive', () {
+      final doc = OrgDocument.parse('''
+#+language: en
+foo
+''');
+      expect(extractLocale(doc), Locale('en'));
+    });
     test('empty', () {
+      final doc = OrgDocument.parse('''
+foo
+''');
+      expect(extractLocale(doc), isNull);
+    });
+  });
+  group('parse', () {
+    test('language', () {
       expect(
-        tryParseLocale(''),
-        isNull,
+        tryParseLocale('en'),
+        Locale('en'),
       );
     });
-    test('too many parts', () {
+    test('language and region', () {
       expect(
-        tryParseLocale('en_Latn_US_foo'),
-        isNull,
+        tryParseLocale('en_US'),
+        Locale('en', 'US'),
       );
     });
-    test('second part bad length', () {
+    test('language and script', () {
       expect(
-        tryParseLocale('en_Lat'),
-        isNull,
+        tryParseLocale('zh_Hans'),
+        Locale.fromSubtags(languageCode: 'zh', scriptCode: 'Hans'),
       );
+    });
+    test('language, script, and region', () {
+      expect(
+        tryParseLocale('zh_Hans_CN'),
+        Locale.fromSubtags(
+          languageCode: 'zh',
+          scriptCode: 'Hans',
+          countryCode: 'CN',
+        ),
+      );
+    });
+    test('hyphens', () {
+      expect(
+        tryParseLocale('zh-Hans-CN'),
+        Locale.fromSubtags(
+          languageCode: 'zh',
+          scriptCode: 'Hans',
+          countryCode: 'CN',
+        ),
+      );
+    });
+    group('invalid', () {
+      test('empty', () {
+        expect(
+          tryParseLocale(''),
+          isNull,
+        );
+      });
+      test('too many parts', () {
+        expect(
+          tryParseLocale('en_Latn_US_foo'),
+          isNull,
+        );
+      });
+      test('second part bad length', () {
+        expect(
+          tryParseLocale('en_Lat'),
+          isNull,
+        );
+      });
     });
   });
 }
