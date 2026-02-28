6835f9ce0b2a56c1b0575390bb0de78061e9afe5
Aaron Madlon-Kay <aaron@madlon-kay.com>
2026-03-01 08:38:25 +0900

    Make block headers/footers participate in search highlighting
    See https://github.com/amake/orgro/issues/195

--- lib/src/widget/org_block.dart
+++ lib/src/widget/org_block.dart
@@ -47,36 +47,40 @@ class _OrgBlockWidgetState extends State<OrgBlockWidget>
     return IndentBuilder(
       widget.block.indent,
       builder: (context, totalIndentSize) {
-        return ValueListenableBuilder<bool>(
-          valueListenable: openListenable,
-          builder: (context, open, child) => Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              _header(context, metaStyle, open: open, hideMarkup: hideMarkup),
-              AnimatedSwitcher(
-                duration: const Duration(milliseconds: 100),
-                transitionBuilder: (child, animation) =>
-                    SizeTransition(sizeFactor: animation, child: child),
-                child: open ? child : const SizedBox.shrink(),
-              ),
-              if (trailing.isNotEmpty)
-                // Remove another line break because the existence of even an
-                // empty string here takes up a line.
-                Text(removeTrailingLineBreak(trailing)),
-            ],
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              _body(context, totalIndentSize),
-              reduceOpacity(
-                Text(
-                  hardDeindent(widget.block.footer, totalIndentSize),
-                  style: metaStyle,
+        return FancySpanBuilder(
+          builder: (context, spanBuilder) => ValueListenableBuilder<bool>(
+            valueListenable: openListenable,
+            builder: (context, open, child) => Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                _header(context, spanBuilder, metaStyle,
+                    open: open, hideMarkup: hideMarkup),
+                AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 100),
+                  transitionBuilder: (child, animation) =>
+                      SizeTransition(sizeFactor: animation, child: child),
+                  child: open ? child : const SizedBox.shrink(),
                 ),
-                enabled: hideMarkup,
-              ),
-            ],
+                if (trailing.isNotEmpty)
+                  // Remove another line break because the existence of even an
+                  // empty string here takes up a line.
+                  Text(removeTrailingLineBreak(trailing)),
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                _body(context, spanBuilder, totalIndentSize),
+                reduceOpacity(
+                  Text.rich(
+                    spanBuilder.highlightedSpan(
+                        hardDeindent(widget.block.footer, totalIndentSize)),
+                    style: metaStyle,
+                  ),
+                  enabled: hideMarkup,
+                ),
+              ],
+            ),
           ),
         );
       },
@@ -85,6 +89,7 @@ class _OrgBlockWidgetState extends State<OrgBlockWidget>
 
   Widget _header(
     BuildContext context,
+    OrgSpanBuilder spanBuilder,
     TextStyle metaStyle, {
     required bool hideMarkup,
     required bool open,
@@ -93,8 +98,8 @@ class _OrgBlockWidgetState extends State<OrgBlockWidget>
     if (!hideMarkup && !open) {
       text += '...';
     }
-    Widget header = Text(
-      text,
+    Widget header = Text.rich(
+      spanBuilder.highlightedSpan(text),
       style: metaStyle,
       softWrap: !hideMarkup,
       overflow: hideMarkup ? TextOverflow.fade : null,
@@ -116,7 +121,8 @@ class _OrgBlockWidgetState extends State<OrgBlockWidget>
     );
   }
 
-  Widget _body(BuildContext context, int indentSize) {
+  Widget _body(
+      BuildContext context, OrgSpanBuilder spanBuilder, int indentSize) {
     final block = widget.block;
     Widget body;
     if (block is OrgSrcBlock) {
@@ -125,63 +131,56 @@ class _OrgBlockWidgetState extends State<OrgBlockWidget>
           removeTrailingLineBreak(softDeindent(codeNode.content, indentSize));
       final refPattern = block.coderefPattern();
       final defaultStyle = DefaultTextStyle.of(context).style;
-      body = FancySpanBuilder(
-        builder: (context, spanBuilder) {
-          InlineSpan span;
-          if (!supportedSrcLanguage(block.language)) {
-            final codeStyle = defaultStyle.copyWith(
-              color: OrgTheme.dataOf(context).codeColor,
-            );
-            span = TextSpan(
-              children: _tokenizeText(code, refPattern).map((t) {
-                return t.coderef == null
-                    ? spanBuilder.highlightedSpan(t.text, style: codeStyle)
-                    : WidgetSpan(
-                        child: OrgCoderefWidget(
-                          t.text,
-                          spanBuilder,
-                          style: codeStyle,
-                          key: OrgLocator.of(context)
-                              ?.generateCoderefKey(t.coderef!),
-                        ),
-                      );
-              }).toList(growable: false),
-            );
-          } else {
-            span = buildSrcHighlightSpan(
-              context,
-              code: code,
-              languageId: block.language,
-              spanFactory: ({String? text, TextStyle? style}) {
-                if (text == null) return const TextSpan();
+      InlineSpan span;
+      if (!supportedSrcLanguage(block.language)) {
+        final codeStyle = defaultStyle.copyWith(
+          color: OrgTheme.dataOf(context).codeColor,
+        );
+        span = TextSpan(
+          children: _tokenizeText(code, refPattern).map((t) {
+            return t.coderef == null
+                ? spanBuilder.highlightedSpan(t.text, style: codeStyle)
+                : WidgetSpan(
+                    child: OrgCoderefWidget(
+                      t.text,
+                      spanBuilder,
+                      style: codeStyle,
+                      key: OrgLocator.of(context)
+                          ?.generateCoderefKey(t.coderef!),
+                    ),
+                  );
+          }).toList(growable: false),
+        );
+      } else {
+        span = buildSrcHighlightSpan(
+          context,
+          code: code,
+          languageId: block.language,
+          spanFactory: ({String? text, TextStyle? style}) {
+            if (text == null) return const TextSpan();
 
-                final refMatch = refPattern.firstMatch(text);
-                if (refMatch == null) {
-                  return spanBuilder.highlightedSpan(text, style: style);
-                }
-                final refKey = refMatch.namedGroup('name')!;
-                return WidgetSpan(
-                  child: OrgCoderefWidget(
-                    text,
-                    spanBuilder,
-                    style: style,
-                    key: OrgLocator.of(context)?.generateCoderefKey(refKey),
-                  ),
-                );
-              },
+            final refMatch = refPattern.firstMatch(text);
+            if (refMatch == null) {
+              return spanBuilder.highlightedSpan(text, style: style);
+            }
+            final refKey = refMatch.namedGroup('name')!;
+            return WidgetSpan(
+              child: OrgCoderefWidget(
+                text,
+                spanBuilder,
+                style: style,
+                key: OrgLocator.of(context)?.generateCoderefKey(refKey),
+              ),
             );
-          }
-          return Text.rich(span);
-        },
-      );
+          },
+        );
+      }
+      body = Text.rich(span);
     } else if (block.body is OrgPlainText) {
       final contentNode = block.body as OrgPlainText;
       final content = removeTrailingLineBreak(
           softDeindent(contentNode.content, indentSize));
-      body = FancySpanBuilder(
-        builder: (context, spanBuilder) =>
-            Text.rich(spanBuilder.highlightedSpan(content)),
-      );
+      body = Text.rich(spanBuilder.highlightedSpan(content));
     } else {
       // This feels a bit costly, but it's the easiest way to handle scenarios
       // where the body is indented *less* than the block delimiters.
