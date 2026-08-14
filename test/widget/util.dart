b2046a0c4c4c6e58b991cc22c623b48d6f59e521
Aaron Madlon-Kay <aaron@madlon-kay.com>
2026-08-14 23:17:52 +0900

    Fix test

--- test/widget/util.dart
+++ test/widget/util.dart
@@ -3,6 +3,12 @@ import 'package:material_ui/material_ui.dart';
 Widget wrap(Widget child) {
   return Directionality(
     textDirection: TextDirection.ltr,
-    child: Material(child: child),
+    // TODO(aaron): Remove this once flutter_slidable is updated to use material_ui
+    // https://github.com/letsar/flutter_slidable/issues/548
+    child: Localizations(
+      delegates: GlobalMaterialLocalizations.delegates,
+      locale: Locale('en', 'US'),
+      child: Material(child: child),
+    ),
   );
 }
